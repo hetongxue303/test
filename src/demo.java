cbbf07e5e78cc4a1d4d30f48e7df86e9b3413dd1
hetongxue303 <1974597686@qq.com>
2023-02-01 15:45:02 +0800

    读写锁

--- src/demo.java
+++ src/demo.java
@@ -1,43 +1,55 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * 独占锁(写锁)：一次只能被一个线程写入
+ * 共享锁(读锁)：多个线程可以同时占有
+ */
 public class demo {
 
     public static void main(String[] args) {
-        /*
-         * ArrayList默认不安全
-         * 解决方案：
-         * 1.使用Vector(不推荐)
-         * 2.Collections.synchronizedList(new ArrayList<>())
-         * 3.new CopyOnWriteArrayList<>()
-         */
-//        List<String> list = new ArrayList<>();
-//        List<String> list = new Vector<>();
-        List<String> list = new CopyOnWriteArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            new Thread(() -> {
-                list.add(UUID.randomUUID().toString().substring(0, 5));
-                System.out.println(list);
-            }, String.valueOf(i)).start();
+        Cache cache = new Cache();
+        for (int i = 0; i <= 5; i++) {
+            int finalI = i;
+            new Thread(() -> cache.put(String.valueOf(finalI), finalI), String.valueOf(i)).start();
         }
-
-        Set<Object> objects = new HashSet<>();
+        for (int i = 0; i <= 5; i++) {
+            int finalI = i;
+            new Thread(() -> cache.get(String.valueOf(finalI)), String.valueOf(i)).start();
+        }
 
     }
 
 }
 
-class Phone {
+class Cache {
+    private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Map<String, Object> map = new HashMap<>();
 
-    public synchronized void sendEmail() {
-        System.out.println("发邮件...");
+    public void put(String key, Object value) {
+        try {
+            readWriteLock.writeLock().lock();// 写入锁
+            System.out.println(Thread.currentThread().getName() + "正在写入" + key);
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "写入完成");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
-    public synchronized void call() {
-        System.out.println("打电话...");
+    public void get(String key) {
+        try {
+            readWriteLock.readLock().lock();// 读锁
+            System.out.println(Thread.currentThread().getName() + "正在读取" + key);
+            Object o = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "读取完成，结果：" + o);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
-
 }
